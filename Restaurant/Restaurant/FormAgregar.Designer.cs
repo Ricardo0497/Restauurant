d36f2b04e33727f6f600dbe117baf3e98d4021b0
Laptop <Laptop@RAAZ>
2020-03-18 20:32:18 -0600

    AvanceSeis

--- Restaurant/Restaurant/FormAgregar.Designer.cs
+++ Restaurant/Restaurant/FormAgregar.Designer.cs
@@ -69,7 +69,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(166, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(160, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(119, 101);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
